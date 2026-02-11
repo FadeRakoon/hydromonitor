a79aaa3777c0c2f4ff5115037ce5c388e2d72e4c
FadeRakoon <110274955+FadeRakoon@users.noreply.github.com>
2026-02-11 13:29:57 -0500

    updated ssid and password

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -50,8 +50,8 @@ static const char* mqtt_server   = "www.yanacreations.com";         // Broker IP
 static uint16_t mqtt_port        = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid       = "Rakoon";     // Add your Wi-Fi ssid
-const char* password   = "i_isARakoon"; // Add your Wi-Fi password 
+const char* ssid       = "MonaConnect";     // Add your Wi-Fi ssid
+const char* password   = ""; // Add your Wi-Fi password 
 
 
 
